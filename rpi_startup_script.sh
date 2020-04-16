cc0204c964cd0d5219e6ce0f97c62651a1f678b7
eugf <eugf@users.noreply.github.com>
2020-04-16 13:22:42 -0400

    Update rpi_startup_script.sh

--- rpi_startup_script.sh
+++ rpi_startup_script.sh
@@ -6,19 +6,29 @@ echo "#################################################"
 echo "~~~~~~~~~~~~~~~~~~INITIALIZING~~~~~~~~~~~~~~~~~~"
 echo "#################################################"
 
-#read my_name
+#Name the new user account
+read $my_name
 sudo adduser $my_name
+
+#Give new user sudo permissions
 sudo usermod -a -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,gpio,i2c,spi $my_name
-#Test if sudo works
+
+#Manual test to see if sudo works
 sudo su - $my_name
+
+#Kill pi user
 sudo pkill -u pi
-#logout, login to new user
+
+#TODO: logout, login to new user
 #???
+
+#Delete the default pi user, remove pi home directory
 sudo deluser pi
 sudo deluser -remove-home pi
-sudo nano /etc/sudoers.d/010_pi-nopasswd
 
-#Then change this line to have your new username
+#Require new user to login with password
+sudo nano /etc/sudoers.d/010_pi-nopasswd
+#TODO: Then change this line to have your new username
 #I'd say sudo touch to create a new file and replace it
 #$my_name ALL=(ALL) PASSWD: ALL
 
@@ -26,6 +36,6 @@ read pass1
 #echo "Please confirm password"
 read pass2
 #check match???
-#echo 
+#echo
 #passwd $pass2
 echo "Your new password has been set"
